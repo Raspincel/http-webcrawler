c79e4a5f2734dc682dfa8e88cc70a6f8a1229791
Raspincel <raspincel@gmail.com>
2023-03-11 02:21:08 -0300

    Added get URLs function and tests

--- crawl.test.ts
+++ crawl.test.ts
@@ -1,6 +1,7 @@
-const normalizeURL = require('./crawl.ts');
+const { normalizeURL, getURLsFromHtml } = require('./crawl.ts');
 const { test, expect } = require('@jest/globals');
 
+// normalize URL tests
 test('normalizeURL strip protocol', ()=> {
     const input = 'https://blog.boot.dev/path';
     const expected = 'blog.boot.dev/path';
@@ -30,5 +31,48 @@ test('normalizeURL strip http protocol', ()=> {
     const expected = 'blog.boot.dev/path';
 
     const received: string = normalizeURL(input);
+    expect(received).toEqual(expected);
+})
+
+// get URL tests
+test('getURLsFromHTML absolute URLs', ()=> {
+    const inputHTMLBody = 
+    `<html>
+        <body>
+            <a href="https://blog.boot.dev/path/">Boot.dev Blog</a>
+        </body>
+    </html>`;
+    const inputBaseURL = "https://blog.boot.dev/path/";
+    const expected = ["https://blog.boot.dev/path/"];
+
+    const received = getURLsFromHtml(inputHTMLBody, inputBaseURL);
+    expect(received).toEqual(expected);
+})
+
+test('getURLsFromHTML relative URLS', ()=> {
+    const inputHTMLBody = 
+    `<html>
+        <body>
+            <a href="/path/">Boot.dev Blog</a>
+        </body>
+    </html>`;
+    const inputBaseURL = "https://blog.boot.dev";
+    const expected = ["https://blog.boot.dev/path/"];
+
+    const received = getURLsFromHtml(inputHTMLBody, inputBaseURL);
+    expect(received).toEqual(expected);
+})
+
+test('getURLsFromHTML invalid URLS', ()=> {
+    const inputHTMLBody = 
+    `<html>
+        <body>
+            <a href="baaah">Link 1</a>
+        </body>
+    </html>`;
+    const inputBaseURL = "https://blog.boot.dev";
+    const expected: string[] = [];
+
+    const received = getURLsFromHtml(inputHTMLBody, inputBaseURL);
     expect(received).toEqual(expected);
 })
--- crawl.ts
+++ crawl.ts
@@ -1,3 +1,36 @@
+const {JSDOM} = require('jsdom');
+
+function getURLsFromHtml(htmlBody: string, baseURL: string) : string[] {
+    const urls: string[] = []
+    
+    const dom = new JSDOM(htmlBody);
+    const links = dom.window.document.querySelectorAll('a');
+    
+    for (const element of links) {
+
+        if (element.href.slice(0, 1) === '/') {
+            try {
+                const urlObj = new URL(baseURL + element.href);
+                urls.push(baseURL + element.href);
+
+            } catch(err: any) {
+                console.log(`Error with relative URL: ${err.message}`);
+            }
+
+        } else {
+            try {
+                const urlObj = new URL(element.href);
+                urls.push(element.href);
+            } catch (err: any) {
+                console.log(`Error with absolute URL: ${err.message}`);
+            }
+        }
+
+    }
+
+    return urls;
+}
+
 function normalizeURL(urlString:string) : string {
     const url = new URL(urlString);
 
@@ -10,6 +43,10 @@ function normalizeURL(urlString:string) : string {
     return url.hostname + url.pathname;
 }
 
-module.exports = normalizeURL;
+module.exports = { 
+    normalizeURL,
+    getURLsFromHtml
+}
 
+// Without this, there are a few errors regarding block-scope. Why? I have no idea
 export{};
